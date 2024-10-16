452e51bae139ddc8af1d958f63e6d848b2a8a6f4
Felipe Salviano <felipesalviano00@hotmail.com>
2024-10-16 12:53:00 -0300

    Alterações de css

--- static/main.js
+++ static/main.js
@@ -18,10 +18,33 @@ function converter() {
         let resultados = document.getElementById("resultados");
         resultados.innerHTML = '';
         for (let [moeda, valor] of Object.entries(data)) {
-            resultados.innerHTML += `<div class="resultado">${moeda}: ${valor.toFixed(2)} $</div>`;
+            let valorFormatado = valor.toLocaleString('en-US', { style: 'currency', currency: getCurrencyCode(moeda) });
+            resultados.innerHTML += `<div class="resultado">${moeda}: ${valorFormatado}</div>`;
         }
     })
     .catch((error) => {
         console.error('Erro:', error);
     });
+}
+
+// Função para mapear as moedas com seus códigos de moeda ISO
+function getCurrencyCode(moeda) {
+    switch(moeda) {
+        case 'Real':
+            return 'BRL';
+        case 'Dollar':
+            return 'USD';
+        case 'Libra esterlina':
+            return 'GBP';
+        case 'Bitcoin':
+            return 'BTC';
+        case 'Etherum':
+            return 'ETH';
+        case 'BNB':
+            return 'BNB';
+        case 'Solana':
+            return 'SOL';
+        default:
+            return 'USD'; // Padrão
+    }
 }
